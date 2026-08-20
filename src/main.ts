eceb91450bdc60ee5898236dd5b02a23e121eb2c
Daniel Houck <Software@DRHouck.me>
2026-08-19 20:28:23 -0400

    Print args
    Expectation: this will not be built

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { print } from "kolmafia";
 
-export function main(): void {
+export function main(args: string): void {
 	print("You have successfully built github-workflow-permissions-test!");
+	print(`You called it with \`${args}\`.`);
 }
